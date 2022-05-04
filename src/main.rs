4fb59f19158fee6384c79242950ba03b77d00837
Lev <lguzm038@uottawa.ca>
2022-05-04 16:10:35 -0500

    Implement read file from input functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     //First, we need to collect the arguments given to the program
@@ -6,4 +7,8 @@ fn main() {
 
     let query = &args[1];
     let file_name = &args[2];
+
+    let error_msg = format!("Could not read file '{}'", file_name);
+    let contents = fs::read_to_string(file_name).expect(&error_msg); //read the contents of the input filename
+    println!("{}", contents);
 }
