c26eb4f6b6f32c15dce848dc99401aed9d8d0f60
Lev <lguzm038@uottawa.ca>
2022-05-06 08:47:36 -0500

    Extract parse config functionality

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,18 @@ fn main() {
     //First, we need to collect the arguments given to the program
     let args: Vec<String> = env::args().collect(); //collect returns an iterator (vector)
 
-    let query = &args[1];
-    let file_name = &args[2];
+    let (query, filename) = parse_config(&args);
 
-    let error_msg = format!("Could not read file '{}'", file_name);
-    let contents = fs::read_to_string(file_name).expect(&error_msg); //read the contents of the input filename
+    let error_msg = format!("Could not read file '{}'", filename);
+    let contents = fs::read_to_string(filename).expect(&error_msg); //read the contents of the input filename.
+
+    //.expect prints the error message in the input instead of the standard panic! message
     println!("{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
